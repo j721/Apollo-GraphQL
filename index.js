6467c6944b6d03b6e7156d4fe81e63d64712f01e
j721 <juliadvu@gmail.com>
2020-10-24 23:35:01 -0500

    created mock database of users

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const {ApolloServer,gql} = require('apollo-server');
 const crypto = require('crypto');
 
+const db ={
+    users:[
+        {id:'1', email: 'judy@gmail.com', name: 'Judy', avatarUrl: 'https://gravatar.com/...' },
+        {id:'1', email: 'Bill@gmail.com', name: 'Bill', avatarUrl: 'https://gravatar.com/...' },
+    ],
+    messages: [
+        {id: '1', userId: '1', body: 'Hello', createdAt: Date.now()},
+        {id: '2', userId: '2', body: 'Hi', createdAt: Date.now()},
+        {id: '3', userId: '1', body: 'What\'s up?', createdAt: Date.now()},
+
+    ]
+}
