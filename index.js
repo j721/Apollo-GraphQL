065a0b7e26008c4b1254d8161eaf13753cd93fc4
j721 <juliadvu@gmail.com>
2020-10-24 23:48:46 -0500

    created a resolvers query

--- index.js
+++ index.js
@@ -1,38 +1,55 @@
-const {ApolloServer,gql} = require('apollo-server');
-const crypto = require('crypto');
+const { ApolloServer, gql } = require("apollo-server");
+const crypto = require("crypto");
 
-const db ={
-    users:[
-        {id:'1', email: 'judy@gmail.com', name: 'Judy', avatarUrl: 'https://gravatar.com/...' },
-        {id:'1', email: 'Bill@gmail.com', name: 'Bill', avatarUrl: 'https://gravatar.com/...' },
-    ],
-    messages: [
-        {id: '1', userId: '1', body: 'Hello', createdAt: Date.now()},
-        {id: '2', userId: '2', body: 'Hi', createdAt: Date.now()},
-        {id: '3', userId: '1', body: 'What\'s up?', createdAt: Date.now()},
-
-    ]
-}
+const db = {
+  users: [
+    {
+      id: "1",
+      email: "judy@gmail.com",
+      name: "Judy",
+      avatarUrl: "https://gravatar.com/...",
+    },
+    {
+      id: "1",
+      email: "Bill@gmail.com",
+      name: "Bill",
+      avatarUrl: "https://gravatar.com/...",
+    },
+  ],
+  messages: [
+    { id: "1", userId: "1", body: "Hello", createdAt: Date.now() },
+    { id: "2", userId: "2", body: "Hi", createdAt: Date.now() },
+    { id: "3", userId: "1", body: "What's up?", createdAt: Date.now() },
+  ],
+};
 
 const typeDefs = gql`
-    type Query{
-        users: [User!]!
-        user(id: ID!): User
-        messages: [Message!]!
-    }
-    type Mutation{
-        addUser(email: String!, name: String): User!
-    }
-    type User{
-        id: ID!
-        email: String!
-        name: String
-        avatarUrl: String
-        messages: [Message!]!
-    }
-    type Message{
-        id: ID!
-        body: String!
-        createdAt: String!
-    }
-    `
+  type Query {
+    users: [User!]!
+    user(id: ID!): User
+    messages: [Message!]!
+  }
+  type Mutation {
+    addUser(email: String!, name: String): User!
+  }
+  type User {
+    id: ID!
+    email: String!
+    name: String
+    avatarUrl: String
+    messages: [Message!]!
+  }
+  type Message {
+    id: ID!
+    body: String!
+    createdAt: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    users: () => db.users, //connect to mock database
+    user: (root, { id }) => db.users.find((user) => user.id === id), //find single user by the id,
+    messages: () =>db.messages, //connect to mock database
+  },
+};
