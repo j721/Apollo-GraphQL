d840cd2884a7f0a3d41e217f36644b054ae4e34c
j721 <juliadvu@gmail.com>
2020-10-27 00:14:43 -0500

    updated environment variable to include port for server to listen to

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
-import {ApolloServer, gql} from 'apollo-server-express';
+import {ApolloServer} from 'apollo-server-express';
 import express from 'express';
+import typeDefs from './typeDefs';
+import resolvers from './resolvers';
+
+const {
+    APP_PORT = 4000, 
+    NODE_ENV ='development'
+} = process.env
 
 const app = express()
+
+app.disable('x-powered-by') //disable one of the headers
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+})
+
+server.applyMiddleware({app})
+
+app.listen({port: 4000},()=>{
+    console.log(`Server ready at http://localhost:${APP_PORT}${server.setGraphQLPath}`)
+})
