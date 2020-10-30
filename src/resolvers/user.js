7f71f60c965281231941998edc71dca095603890
Julia.Vu <julia.vu@bumpboxes.com>
2020-10-30 00:13:15 -0500

    Layout for user resolvers made

---
+++ src/resolvers/user.js
@@ -0,0 +1,15 @@
+export default{
+    Query:{
+        users: (root, arg, context, info)=>{
+
+        },
+        users: (root, arg, context, info)=>{
+
+        },
+        Mutation:{
+            signUp: (root, arg, context, info)=>{
+                
+            }
+        }
+    }
+}
